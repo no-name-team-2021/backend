22ba388d3c64d44d04a12c6e458b6a4687262bdf
维帆 刘 <1798707219@qq.com>
2021-07-09 15:09:40 +0800

    基本框架v2 test

--- user/user-web/src/main/java/cn/edu/whu/shopping/user/controller/UserController.java
+++ user/user-web/src/main/java/cn/edu/whu/shopping/user/controller/UserController.java
@@ -1,8 +1,11 @@
 package cn.edu.whu.shopping.user.controller;
 
+import org.springframework.stereotype.Controller;
+
 /**
  * @Author LiuWeiFan
  * @create 2021/7/9 14:08
  */
+@Controller
 public class UserController {
 }
